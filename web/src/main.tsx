94ab9a7f751c694b3f5586b4edaff8c0c45a53ca
David <dfl8@aluno.ifal.edu.br>
2024-05-09 23:38:22 -0300

    feat(main): add ThemeProvider from styled-components

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes } from './routes'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Routes/>
+    <ThemeProvider theme={theme}>
+      <Routes/>
+    </ThemeProvider>
   </React.StrictMode>,
 )

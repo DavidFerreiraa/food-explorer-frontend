189bf8c15d7f8a4891850bcb4502284a18c55678
David <dfl8@aluno.ifal.edu.br>
2024-05-18 22:02:24 -0300

    feat(components): add fileState to input type file.

--- web/src/components/FormField/index.tsx
+++ web/src/components/FormField/index.tsx
@@ -1,15 +1,24 @@
 import { InputHTMLAttributes } from "react";
 import { Input, Label, Container } from "./styles";
+import { LuUpload } from "react-icons/lu";
+import { FaCheckCircle } from "react-icons/fa";
 
 interface IFormField extends InputHTMLAttributes<HTMLInputElement> {
-    label: string
+    label: string,
+    htmlFor: string,
+    fileState?: "file" | "fileSelected"
 }
 
-export function FormField({label, ...rest}: IFormField) {
+export function FormField({label, htmlFor, fileState = "file", ...rest}: IFormField) {
     return(
         <Container>
-            <Label htmlFor={label}>{label}</Label>
-            <Input {...rest}/>
+            <Label htmlFor={htmlFor}>{label}</Label>
+            <Label className={fileState} htmlFor={htmlFor}>
+                <LuUpload size={24} data-inputstate="upload"/>
+                <FaCheckCircle size={24}data-inputstate="saved"/>
+                {label}
+            </Label>
+            <Input {...rest} name={htmlFor} id={htmlFor}/>
         </Container>
     );
 }
--- web/src/components/FormField/styles.ts
+++ web/src/components/FormField/styles.ts
@@ -2,19 +2,91 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    
     display: flex;
     flex-direction: column;
 
-    gap: clamp(.4rem, .8rem, .16rem);
+    height: 4.8rem;
+
+    gap: 1.6rem;
+
+    input[type="file"] {
+        display: none;
+    }
+
+    label.file, label.fileSelected{
+        display: none;
+    };
+
+    &:has(input[type="file"]) {
+        label.file {
+            > [data-inputstate="saved"] {
+                display: none;
+            }
+
+            display: flex;
+            align-self: start;
+            gap: .8rem;
+
+            font-family: "Poppins", "Roboto", sans-serif;
+            font-size: 1.4rem;
+            font-weight: 500;
+            line-height: 2.4rem;
+
+            color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+            background-color: ${({ theme }) => theme.COLORS.DARK_800};
+
+            border-radius: .8rem;
+
+            padding: 1.2rem 3.2rem;
+
+            cursor: pointer;
+        }
+
+        label.fileSelected {
+            > [data-inputstate="upload"] {
+                display: none;
+            }
+
+            display: flex;
+            align-self: start;
+            gap: .8rem;
+
+            font-family: "Poppins", "Roboto", sans-serif;
+            font-size: 1.4rem;
+            font-weight: 500;
+            line-height: 2.4rem;
+
+            color: ${({ theme }) => theme.COLORS.MINT_100};
+
+            background-color: ${({ theme }) => theme.COLORS.DARK_800};
+
+            border-radius: .8rem;
+
+            padding: 1.2rem 3.2rem;
+
+            cursor: pointer;
+        }
+    }
+
+    input[type=number]{ //remove arrows from input type number in mozzila browser
+        -moz-appearance: textfield;
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button { //remove arrow from input type number in all others browsers
+    -webkit-appearance: none;
+    margin: 0;
+    }
 `;
 
 export const Input = styled.input`
+
     display: flex;
 
-    padding: 1.2rem 1.4rem;
+    padding: 1.3rem 1.4rem;
 
-    background: ${({ theme }) => theme.COLORS.DARK_900};
+    background: ${({ theme }) => theme.COLORS.DARK_800};
     
     border: ${({ theme }) => `1px solid ${theme.COLORS.DARK_900}`};
     border-radius: 5px;
@@ -24,7 +96,7 @@ export const Input = styled.input`
     &::placeholder {
         font-family: "Roboto", "Poppins", sans-serif;
         font-weight: 400;
-        font-size: clamp(1rem, 1.2rem, 1.6rem);
+        font-size: 1.6rem;
         line-height: 100%;
 
         color: ${({ theme }) => theme.COLORS.LIGHT_500}
@@ -36,10 +108,11 @@ export const Input = styled.input`
 `;
 
 export const Label = styled.label`
+    align-self: start;
     font-family: "Roboto", "Poppins", sans-serif;
     font-weight: 400;
     font-size: clamp(1.2rem, 1.6rem, 2rem);
     line-height: 100%;
     
-    color: ${({ theme }) => theme.COLORS.LIGHT_400}
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
 `;

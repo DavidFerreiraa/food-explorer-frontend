8a203df9f8500b40e932d870c8532e64c0a2c4d0
David <dfl8@aluno.ifal.edu.br>
2024-05-18 22:10:01 -0300

    feat(LogOut): add handleSignOut

--- web/src/components/LogOut/index.tsx
+++ web/src/components/LogOut/index.tsx
@@ -1,12 +1,19 @@
 import { GoSignOut } from "react-icons/go";
 import { Container } from "./styles";
 import { useAuth } from "../../hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 export function LogOut() {
     const { signOut } = useAuth();
+    const navigator = useNavigate();
+
+    function handleSignOut() {
+        signOut();
+        navigator("/");
+    }
 
     return(
-        <Container onClick={signOut}>
+        <Container onClick={handleSignOut}>
             <GoSignOut size={32}/>
         </Container>
     );

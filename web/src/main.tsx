6f3775195fc9f52482a85d1d24f3a7127649e5ac
David <dfl8@aluno.ifal.edu.br>
2024-05-18 22:17:02 -0300

    feat(main): add ProductProvider.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,14 +7,17 @@ import GlobalStyles from './styles/global';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from './hooks/auth'
+import { ProductProvider } from './hooks/product'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <GlobalStyles/>
-        <Routes/>
-        <ToastContainer theme='dark'/>
+        <ProductProvider>
+          <GlobalStyles/>
+          <Routes/>
+          <ToastContainer theme='dark'/>
+        </ProductProvider>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,

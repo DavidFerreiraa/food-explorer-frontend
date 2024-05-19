3330d6cf5258f45f6177696e373283d8b53b0fd1
David <dfl8@aluno.ifal.edu.br>
2024-05-18 22:16:09 -0300

    feat(routes -> admin.routes): add NewProduct route.

--- web/src/routes/admin.routes.tsx
+++ web/src/routes/admin.routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { NewProduct } from "../pages/NewProduct";
 
 export function AdminRoutes() {
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/newproduct" element={<NewProduct/>}/>
         </Routes>
     );
 }

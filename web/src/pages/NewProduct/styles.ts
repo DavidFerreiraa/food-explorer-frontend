6a15cf9d3360f3f27b45d7d0e0ff69e6daa7999c
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-14 22:37:01 -0300

    style: add a paddig to the container

--- web/src/pages/NewProduct/styles.ts
+++ web/src/pages/NewProduct/styles.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100%;
     height: 100%;
+    padding: 3.2rem 12.2rem;
 
-    padding: 0 12.4rem;
 
     background-color: ${({ theme }) => theme.COLORS.DARK_100};
 `;

2d661b7f2914f7daffb868b95068d5e87c70b93c
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-14 22:41:21 -0300

    style: add the wrappers to user.routes

--- web/src/routes/user.routes.tsx
+++ web/src/routes/user.routes.tsx
@@ -3,16 +3,19 @@ import { Home } from "../pages/Home";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { ShowProduct } from "../pages/ShowProduct";
+import { ContentWrapper, LayoutWrapper } from "./styles";
 
 export function UserRoutes() {
     return(
-        <>
+        <LayoutWrapper>
             <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/showproduct/:id" element={<ShowProduct/>}/>
-            </Routes>
+            <ContentWrapper>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/showproduct/:id" element={<ShowProduct/>}/>
+                </Routes>
+            </ContentWrapper>
             <Footer/>
-        </>
+        </LayoutWrapper>
     );
 }

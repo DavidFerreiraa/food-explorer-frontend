7ae9c3b5349b794ee03716b70d1c4b2182f93de2
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-19 00:51:40 -0300

    feat: add IORderProducts interface

--- web/src/interfaces/IOrder.ts
+++ web/src/interfaces/IOrder.ts
@@ -1,18 +1,17 @@
 import { IProduct } from "./IProduct"
 
+export interface IOrderProducts {
+    id: string,
+    productId: string,
+    orderId: string,
+    quantity: number,
+    Product: IProduct
+}
 export interface IOrder {
     id: string,
     totalPrice: string,
     ownerId: string,
     createdAt: string,
     updatedAt: string,
-    OrderProducts: [
-        {
-            id: string,
-            productId: string,
-            orderId: string,
-            quantity: number,
-            Product: IProduct
-        }
-    ]
+    OrderProducts: IOrderProducts[]
 }

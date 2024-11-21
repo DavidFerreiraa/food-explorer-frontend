e8a61e40ce3593a3662e1c265d125fc9b57ed56c
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-20 23:49:15 -0300

    feat: add CategoryLists to render when SearchInput in Menu screen

--- web/src/pages/Menu/index.tsx
+++ web/src/pages/Menu/index.tsx
@@ -1,29 +1,51 @@
-import { SearchInput } from "../../components/SearchInput";
-import { useProduct } from "../../hooks/product";
-import { Container, MenuItem, MenuItemWrapper } from "./styles";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/auth";
+import { useCategory } from "../../hooks/category";
+import { useProduct } from "../../hooks/product";
+import { ICategory } from "../../interfaces/ICategory";
+import { Container, MenuItem, MenuItemWrapper } from "./styles";
+import { SearchInput } from "../../components/SearchInput";
+import { CategoryList } from "../../components/CategoryList";
 
 export function Menu() {
-    const { fetchProducts } = useProduct();
+    const { products, fetchProducts } = useProduct();
     const { user, signOut } = useAuth();
-    
+    const { fetchCategories } = useCategory();
+    const [ categories, setCategories ] = useState<ICategory[] | undefined>(undefined);
+    const [ search, setSearch ] = useState<string>("");
+
     async function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearch(event.target.value)
+        console.log(products && search.length > 0)
         event.preventDefault();
         if (event.target.value.length > 0) {
             fetchProducts(event.target.value)
         }
     }
 
+    useEffect(() => {
+        fetchCategories().then((categories) => {
+            setCategories(categories);
+        });
+    }, [])
+
     return(
         <Container>
             <SearchInput placeholder="Busque por pratos ou ingredientes" onChange={(e) => {handleSearch(e)}}/>
-                <MenuItemWrapper>
-                    {
-                        user?.Role === "ADMIN" &&
-                        <MenuItem to="/newproduct">
-                            Novo prato
-                        </MenuItem>
-                    }
+                {
+                    (products && search.length > 0) ? (
+                        categories && categories.map((category) => (
+                            <CategoryList key={category.id} category={category}/>
+                        ))
+                    )
+                    :
+                    (<MenuItemWrapper>
+                        {
+                            user?.Role === "ADMIN" &&
+                            <MenuItem to="/newproduct">
+                                Novo prato
+                            </MenuItem>
+                        }
                     <MenuItem to="/favorites">
                         Meus favoritos
                     </MenuItem>
@@ -33,7 +55,8 @@ export function Menu() {
                     <MenuItem to="/" onClick={signOut}>
                         Sair
                     </MenuItem>
-                </MenuItemWrapper>
+                </MenuItemWrapper>)
+                }
         </Container>
     );
 }
